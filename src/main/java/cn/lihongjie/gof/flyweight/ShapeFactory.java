2f081c3fbf4c0b75e65e9f5d6156944bc85d9349
lihongjie <hongjie.li@boolin.cn>
2019-10-28 09:55:27 +0800

    享元模式注释

--- src/main/java/cn/lihongjie/gof/flyweight/ShapeFactory.java
+++ src/main/java/cn/lihongjie/gof/flyweight/ShapeFactory.java
@@ -2,6 +2,9 @@ package cn.lihongjie.gof.flyweight;
 
 import java.util.HashMap;
 
+/**
+ * 封装对象的创建过程， 提供缓存功能， 使用场景是 大量小对象 + 不可变对象
+ */
 public class ShapeFactory {
     private static final HashMap<String, Shape> circleMap = new HashMap<>();
 
