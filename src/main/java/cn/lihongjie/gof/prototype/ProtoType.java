8dc7f7aea1da1cc6b9b77b26ce7ea060ae73e938
lihongjie <hongjie.li@boolin.cn>
2019-10-28 11:12:20 +0800

    原型模式注释

--- src/main/java/cn/lihongjie/gof/prototype/ProtoType.java
+++ src/main/java/cn/lihongjie/gof/prototype/ProtoType.java
@@ -2,6 +2,16 @@ package cn.lihongjie.gof.prototype;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * 克隆一个对象
+ * 已有一个复杂的对象，需要创建一个类似的新对象
+ *
+ * 把原有的克隆一个，再编辑
+ *
+ *
+ * 为什么要这么做：应为再次从头创建代价比较大
+ * @param <T>
+ */
 public interface ProtoType<T> {
 
 
